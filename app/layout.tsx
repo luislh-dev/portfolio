f470f5767993cafc31c34627f83e3e27a753bc5c
luislopez-developer <luislopezhuari27@gmail.com>
2025-02-07 18:46:00 -0500

    Feat: Agregar GoogleAnlytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import Navigation from '@components/navigation';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import clsx from 'clsx';
 import { JetBrains_Mono, Plus_Jakarta_Sans } from 'next/font/google';
 import Provider from 'providers';
 
 import QuickAccess from '@components/QuickAccess';
 import Shortcuts from '@components/Shortcuts';
+import { GA_ID } from '@config/Google';
 import '@styles/main.css';
 
 const jetbrainsMono = JetBrains_Mono({
@@ -29,6 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <main>{children}</main>
           </div>
         </Provider>
+        <GoogleAnalytics gaId={GA_ID} />
       </body>
     </html>
   );
