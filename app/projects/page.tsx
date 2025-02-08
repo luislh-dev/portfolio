dd6039a16ee45db1463f3df4a070f0c164052002
luislopez-developer <luislopezhuari27@gmail.com>
2025-02-08 15:54:34 -0500

    Feat: Agregar páginas de experiencia y proyectos con mensaje de construcción

---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className='background-grid background-grid--fade-out flex h-screen w-screen items-center justify-center'>
+      <h1 className='text-4xl font-bold'>🚧 En construcción... 🚧</h1>
+    </div>
+  );
+}
